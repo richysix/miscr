c005275906c2cee6c32d51abe52c9d142ffa0ab0
Richard White <rjw26@cam.ac.uk>
2020-04-27 15:55:08 +0100

    Move cluster function from rnaseqVis

---
+++ R/clusterMatrix.R
@@ -0,0 +1,74 @@
+#' hierarchical clustering
+#'
+#' \code{cluster} hierarchically clusters the supplied matrix by column and
+#' returns the reordered matrix
+#'
+#' @param mat Matrix of counts to cluster
+#' @param scale logical indicating whether to center and scale the data
+#' @param dist_method character method for ceating the distance matrix
+#' @param ... other arguments passed to hclust
+#'
+#' @return matrix with columns reordered based on clustering
+#'
+#' @examples
+#' set.seed(1452)
+#' cluster( matrix(rnorm(100), ncol = 5) )
+#'
+#' @export
+#'
+cluster <- function(mat, scale = TRUE, dist_method = "pearson", ...){
+  # centre and scale numbers
+  if (scale) {
+    scaled_matrix <- scale(mat)
+  } else {
+    scaled_matrix <- mat
+  }
+
+  # create distance matrix
+  if (dist_method == "pearson") {
+    distance_matrix <- stats::as.dist( (1 - stats::cor(scaled_matrix))/2 )
+  } else if (dist_method == "spearman") {
+    distance_matrix <- stats::as.dist( (1 - stats::cor(scaled_matrix, method = "spearman"))/2 )
+  } else { # assume euclidian distance
+    distance_matrix <- dist(t(scaled_matrix))
+  }
+  # cluster and reorder correlation matrix
+  clustering <- stats::hclust(distance_matrix, ...)
+
+  return( mat[ , clustering$order] )
+}
+
+#' hierarchical cluster matrix by rows or columns (or both)
+#'
+#' \code{cluster_matrix} hierarchically clusters the supplied matrix by row or column
+#' or both
+#'
+#' @param mat Matrix of counts to cluster
+#' @param by_row logical - whether to cluster the rows of the matrix
+#' @param by_col logical - whether to cluster the columns of the matrix
+#' @param scale logical indicating whether to center and scale the data
+#' @param dist_method character method for ceating the distance matrix
+#' @param ... other arguments passed to hclust
+#'
+#' @return reordered matrix based on clustering
+#'
+#' @examples
+#' set.seed(1452)
+#' cluster_matrix( matrix(rnorm(100), ncol = 5) )
+#'
+#' cluster_matrix( matrix(rnorm(100), ncol = 5), by_row = FALSE, by_col = TRUE )
+#'
+#' @export
+#'
+cluster_matrix <- function(mat, by_row = TRUE, by_col = FALSE,
+                           scale = TRUE, dist_method = "pearson", ...){
+  # cluster columns
+  if( by_col ){
+    mat <- cluster(mat, scale = scale, dist_method = dist_method, ...)
+  }
+  # cluster rows
+  if( by_row ){
+    mat <- t( cluster( t(mat), scale = scale, dist_method = dist_method, ... ) )
+  }
+  return( mat )
+}
---
+++ tests/testthat/test-cluster.R
@@ -0,0 +1,35 @@
+context('clustering')
+library(miscr)
+
+set.seed(63738)
+test_matrix <-
+  matrix(
+    replicate(100, c(sample(1:20, 3), sample(40:60, 3), sample(80:120, 3))),
+    nrow = 100, byrow = TRUE)
+dimnames(test_matrix) <- list("genes" = paste0('gene_', 1:100),
+                              "samples" = paste('sample_', 1:9))
+pearson_clustered_columns <-
+  c("sample_ 4", "sample_ 7", "sample_ 3", "sample_ 1", "sample_ 9",
+    "sample_ 6", "sample_ 8", "sample_ 2", "sample_ 5")
+pearson_clustered_columns_ward_d2 <-
+  c("sample_ 4", "sample_ 7", "sample_ 2", "sample_ 5", "sample_ 3",
+    "sample_ 1", "sample_ 9", "sample_ 6", "sample_ 8")
+euclidean_clustered_columns <-
+  c("sample_ 9", "sample_ 7", "sample_ 8", "sample_ 2", "sample_ 1",
+    "sample_ 3", "sample_ 6", "sample_ 4", "sample_ 5")
+
+test_that("cluster", {
+  expect_equal(colnames(cluster(test_matrix)), pearson_clustered_columns)
+  expect_equal(colnames(cluster(test_matrix, scale=FALSE, dist_method = "euclidean")),
+               euclidean_clustered_columns)
+  expect_equal(colnames(cluster(test_matrix, method = "ward.D2")),
+               pearson_clustered_columns_ward_d2)
+})
+
+test_that("cluster_matrix", {
+  expect_equal(colnames(cluster_matrix(test_matrix, by_col = TRUE)), pearson_clustered_columns)
+  expect_equal(colnames(cluster_matrix(test_matrix, by_col = TRUE, scale=FALSE, dist_method = "euclidean")),
+               euclidean_clustered_columns)
+  expect_equal(colnames(cluster_matrix(test_matrix, by_col = TRUE, method = "ward.D2")),
+               pearson_clustered_columns_ward_d2)
+})
